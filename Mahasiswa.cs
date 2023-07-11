9eb0474551851f2ede87d32d74bcc864c0407cb3
ridhozahranp <ridhozahranp@students.amikom.ac.id>
2023-07-11 11:12:55 +0700

    first commit

---
+++ Mahasiswa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulation_static
+{
+    internal class Mahasiswa
+    {
+
+        private static int jumlahMahasiswa = 0;
+        private int nim;
+        private string nama;
+        private int umur;
+
+        public Mahasiswa(int nim, string nama, int umur)
+        {
+            this.nim = nim;
+            this.nama = nama;
+            this.umur = umur;
+            jumlahMahasiswa++; 
+        }
+
+        public static int JumlahMahasiswa 
+        {
+            get { return jumlahMahasiswa; }
+        }
+
+        public int NIM 
+        {
+            get { return nim; }
+        }
+
+        public string Nama 
+        {
+            get { return nama; }
+        }
+
+        public int Umur 
+        {
+            get { return umur; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulation_static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs1 = new Mahasiswa(22_11_4872, "Ridho Zahran", 20);
+            Mahasiswa mhs2 = new Mahasiswa(22_11_4831, "Ryo", 19);
+            Mahasiswa mhs3 = new Mahasiswa(22_11_4822, "Linggar", 21);
+
+            Console.WriteLine("Jumlah mahasiswa: {0}", Mahasiswa.JumlahMahasiswa); 
+
+            Console.WriteLine("Mahasiswa 1:\n NIM {0}\n Nama {1}\n Umur {2}\n", mhs1.NIM, mhs1.Nama, mhs1.Umur); 
+            Console.WriteLine("Mahasiswa 2:\n NIM {0}\n Nama {1}\n Umur {2}\n", mhs2.NIM, mhs2.Nama, mhs2.Umur);
+            Console.WriteLine("Mahasiswa 3:\n NIM {0}\n Nama {1}\n Umur {2}\n", mhs3.NIM, mhs3.Nama, mhs3.Umur);
+
+            Console.ReadKey();
+        }
+    }
+}
